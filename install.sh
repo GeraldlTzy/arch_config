9eca0752231a8a83acd94b0a9ee24401af9fc805
GeraldlTzy <geraldcalderon016@gmail.com>
2025-10-29 18:34:13 -0600

    config

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env zsh
+CONFIG_HOME="$HOME/git/arch_config"
+ZSH_SOURCE="$HOME/git/arch_config/zsh/zshrc"
+ZSH="$HOME/.zshrc"
+
+#ln -s "$ZSH_SOURCE" "$ZSH"
+#ln -s "$CONFIG_HOME/hypr" "$HOME/.config/hypr"
+#ln -s "$CONFIG_HOME/btop" "$HOME/.config/btop"
+#ln -s "$CONFIG_HOME/ghostty" "$HOME/.config/ghostty"
+#ln -s "$CONFIG_HOME/waybar" "$HOME/.config/waybar"
+#ln -s "$CONFIG_HOME/wofi" "$HOME/.config/wofi"
+#ln -s "$CONFIG_HOME/kitty" "$HOME/.config/kitty"
+#ln -s "$CONFIG_HOME/zellij" "$HOME/.config/zellij"
+
+ln -s "$CONFIG_HOME/nvim" "$HOME/.config/nvim"
